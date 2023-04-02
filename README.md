1b5ec6254d531f14621cfebd7542bd9394566ef4
Vendula Krajíčková <vendysacek@seznam.cz>
2023-04-02 07:38:44 +0200

    Hotový úkol

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# czechitas-lesson11-registrace
+# Cykly - Lekce 11
+
+Domácí úkol pro kurz JavaScript 1 od Czechitas.
+
+## 1. Registrace 
+
+Stáhněte si [připravenou stránku](https://github.com/Czechitas-podklady-WEB/cviceni-registrace/archive/refs/heads/main.zip) s registračním formulářem pro nového uživatele. Doplňte do stránky JavaScriptový kód tak, aby byly splněny následující požadavky.
+
+- Pokud uživatel zadá uživatelské jméno, které je již obsaženo v poli `users`, vypište do prvku s třídou `reg-form__error` chybovou hlášku ve smyslu, že zadané uživatelské jméno je již zabráno.
+- Zkontrolujte, že heslo zadané do prvního políčka je dostatečně bezpečné. Heslo považujeme za bezpečné, pokud má alespoň 10 znaků nebo obsahuje alespoň jeden ze znaků pomlčka `-`, podtržítko `_` nebo dvojtečka `:`.
---
+++ index.js
@@ -0,0 +1,78 @@
+const users = [
+	'petr',
+	'jana-34',
+	'husky16',
+	'66beruska',
+	'thegodofU',
+	'rene_gade',
+	'tommy',
+	'luciluci',
+	'tanko',
+	'poltergeist',
+	'hlava-havla',
+	'telo-ramba',
+]
+
+const regFormElm = document.querySelector('.reg-form');
+const usernameElm = document.querySelector('#username');
+const pass1Elm = document.querySelector('#pass1');
+const pass2Elm = document.querySelector('#pass2');
+const errorElm = document.querySelector('.reg-form__error');
+const newElm = document.createElement("div");
+
+const process = (event) => {
+    event.preventDefault();
+
+	if (usernameElm.value === '') {
+		errorElm.textContent = 'Vyplňte uživatelské jméno.';
+		return;
+	} else if (users.includes(usernameElm.value)) {
+		errorElm.innerHTML = `Uživatel <strong>„${username.value}“</strong> již existuje.`;
+		return;
+	}
+
+	validator(pass1Elm.value);
+	conformity(pass2Elm.value);
+
+	errorElm.textContent = ' ';
+	console.log('OK');
+	newElm.style.color = 'green';
+	newElm.innerHTML = `Registrace uživatele <strong>„${usernameElm.value}“</strong> proběhla úspěšně.`;
+	errorElm.after(newElm);
+	usernameElm.value = pass1Elm.value = pass2Elm.value = '';
+	pass1Elm.classList.remove('akcept');
+}
+
+regFormElm.addEventListener('submit', process);
+
+const validator = () => {
+	if (pass1Elm.value === '') {
+		errorElm.textContent = 'Vytvořte heslo.';
+	} else if (pass1Elm.value.length < 10 || (!pass1Elm.value.includes('-') && !pass1Elm.value.includes('_') && !pass1Elm.value.includes(':'))) {
+        pass1Elm.classList.add('error');
+        pass1Elm.classList.remove('akcept');
+		errorElm.textContent = 'Heslo není bezpečné.';
+        console.log('KO');
+    } else {
+        pass1Elm.classList.remove('error');
+        pass1Elm.classList.add('akcept');
+        console.log('OK')
+		errorElm.style.color = 'green';
+		errorElm.textContent = 'Heslo je dostatečně bezpečné.';
+    }
+    console.log(pass1Elm.value);
+}
+
+pass1Elm.addEventListener('input', validator);
+
+const conformity = () => {
+    if (pass2Elm.value === '' || (pass2Elm.value !== pass1Elm.value)) {
+		errorElm.textContent = 'Zadaná hesla se neshodují.';
+        console.log('KO');
+    } else {
+		errorElm.textContent = ' ';
+	}
+    console.log(pass2Elm.value);
+}
+
+pass2Elm.addEventListener('input', conformity);
